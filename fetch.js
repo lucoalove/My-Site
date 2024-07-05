68c5c6f31ab5c69d9140fb49cd8a966253c96c18
blue🐰 <75032462+BlueBunnex@users.noreply.github.com>
2024-07-05 13:32:36 -0400

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,9 @@
+async function fetchData() {
+    fetch('data.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("HTTP error " + response.status);
+        }
+        return response.json();
+    });
+}
