a4e0be670eb691899fdfd65160aa6d70186fa36e
blue🐰 <75032462+BlueBunnex@users.noreply.github.com>
2024-11-13 04:36:40 -0500

    Update prompt_sanitizer.js

--- prompt_sanitizer/prompt_sanitizer.js
+++ prompt_sanitizer/prompt_sanitizer.js
@@ -1,8 +1,9 @@
 function sanitize_prompt(prompt) {
 
-    while (prompt.includes("()")) {
+    // remove any instances of "((()))" or "( )" and similar
+    while (prompt.includes(/(\( +\))/g)) {
         
-        prompt = prompt.replace(/()/g, "");
+        prompt = prompt.replace(/(\( +\))/g, "");
     }
 
     return prompt;
