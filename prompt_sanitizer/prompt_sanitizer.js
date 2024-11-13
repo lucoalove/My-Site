f894c414893870fc7c5955163d07b4aba146e68d
blue🐰 <75032462+BlueBunnex@users.noreply.github.com>
2024-11-13 04:48:27 -0500

    Update prompt_sanitizer.js

--- prompt_sanitizer/prompt_sanitizer.js
+++ prompt_sanitizer/prompt_sanitizer.js
@@ -2,11 +2,18 @@
 
 function sanitize_prompt(prompt) {
 
+    // collapse concurrent spaces
+    prompt = prompt.replace(/ +/g, " ");
+
     // remove any instances of "((()))" or "( )" and similar
     while (prompt.match(/(\( +\))/g) != null) {
         
         prompt = prompt.replace(/(\( +\))/g, "");
     }
+
+    // replace concurrent commas (even separated by spaces) with a single comma followed by (an arbitrary amount of) spaces
+    prompt = prompt.replace(/ +,/g, ",");   // remove spaces preceding commas
+    prompt = prompt.replace(/,+/g, ",");    // collapse sequences of commas to one comma
 
     return prompt;
 }
